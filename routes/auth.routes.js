c8d3e8ab1c5cf3747c4ac4010bd596cb8185176e
Dorin Ilusca <dorin.ilusca@hotmail.com>
2022-06-17 16:49:53 +0300

    user model and auth

---
+++ routes/auth.routes.js
@@ -0,0 +1,71 @@
+const Router = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+
+const router = new Router();
+
+router.post(
+  "/registration",
+  [
+    check("email", "Incorrect email").isEmail(),
+    check("password", "Password must be 3-12 characters").isLength({
+      min: 3,
+      max: 12,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      console.log(req.body);
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: errors });
+      }
+      const { email, password } = req.body;
+      const candidate = await User.findOne({ email });
+      if (candidate) {
+        return res
+          .status(400)
+          .json({ message: `User with email ${email} already exists` });
+      }
+      const hashPassword = await bcrypt.hash(password, 5);
+      const user = new User({ email, password: hashPassword });
+      await user.save();
+      return res.json({ message: "User was created" });
+    } catch (error) {
+      console.log(error);
+      res.send({ message: "Server error" });
+    }
+  }
+);
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+    const isPassValid = bcrypt.compareSync(password, user.password);
+    if (!isPassValid) {
+      return res.status(400).json({ message: "Wrong password" });
+    }
+    const token = jwt.sign({ id: user.id }, config.get("secretKey"), {
+      expiresIn: "1h",
+    });
+    return res.json({
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        diskSpace: user.diskSpace,
+        usedSpace: user.usedSpace,
+        avatar: user.avatar,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.send({ message: "Server error" });
+  }
+});
+
+module.exports = router;
